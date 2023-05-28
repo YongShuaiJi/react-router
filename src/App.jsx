94b8c3a9b08144cff45d14918e2871c5b25760f7
须臾 <jysnana@163.com>
2023-05-28 16:44:13 +0800

    路由的基本使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
+import {Link,Route} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
 
 export default class App extends Component {
   
   render() {
     return (
-      <div className='container'>
-        App...
+      <div id='root'>
+        <div>
+          <div className="row">
+            <div className="col-xs-offset-2 col-xs-8">
+              <div className="page-header"><h2>React Router Demo</h2></div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-2 col-xs-offset-2">
+              <div className="list-group">
+              {/* React中由连接实现切换组件--编写路由链接 */}
+                <Link className="list-group-item" to="/about">About</Link>
+                <Link className="list-group-item" to="/home">Home</Link>
+              </div>
+            </div>
+            <div className="col-xs-6">
+              <div className="pane1">
+                <div className="pane1-body">
+                {/* 注册路由 */}
+                  <Route path="/about" component={About}></Route>
+                  <Route path="/home" component={Home}></Route>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
